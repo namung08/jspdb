4e5d3d1d193804afbeb361d2732cf1b8a165cff6
namung08 <namung08@gmail.com>
2024-02-21 14:44:11 +0900

    jsp.sql

--- workspace6/General/Scripts/jsp/jsp.sql
+++ workspace6/General/Scripts/jsp/jsp.sql
@@ -1,10 +1,12 @@
+-- 테이블 만들기
 CREATE TABLE car (
 	carnum varchar2(20) PRIMARY KEY,
 	brand varchar2(200),
 	color varchar2(200),
 	price number(10)
 );
+-- 만들어진 테이블 확인
 SELECT * FROM car;
-
+-- 테이블에 데이터 넣기
 INSERT INTO car VALUES ('1','Benz','black',350000000);
 INSERT INTO car VALUES ('2','Benz','White',350000000);
