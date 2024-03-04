06cfea12111e39f8354927a039e61388ed82f829
namung08 <namung08@gmail.com>
2024-03-04 16:32:00 +0900

    ?

--- workspace6/General/Scripts/jsp/jsp.sql
+++ workspace6/General/Scripts/jsp/jsp.sql
@@ -63,22 +63,57 @@ ALTER TABLE TBL_MEMBER  ADD (
 SELECT * FROM TBL_MEMBER ;
 
 
+CREATE TABLE test (
+	 intdata number(10),
+	 strdata varchar2(300)
+);
+
+SELECT * FROM TEST;
+INSERT INTO test VALUES (99,'admin');
+
+
+CREATE TABLE "USER"(
+	userid varchar2(300) PRIMARY KEY,
+	userpw varchar2(300),
+	username varchar2(300),
+	userphone varchar2(300),
+	useraddr	varchar2(1000)
+);
+SELECT * FROM "USER";
 
 
 
 
+----------------------------------
+-- board table 생성
+CREATE TABLE tbl_board (
+	boardnum number(10) PRIMARY KEY,
+	boardtitle varchar2(300),
+	boardcontents varchar2(4000),
+	username varchar2(300),
+	boarddate DATE,
+	boardreadcount number(10)
+);
+SELECT * FROM TBL_BOARD ORDER BY BOARDNUM desc;
 
+CREATE SEQUENCE board_seq
+START WITH 1
+INCREMENT BY 1;
+DROP SEQUENCE board_seq;
+INSERT INTO tbl_board VALUES (board_seq.nextval, '첫 번째 글', '첫 번째 내용', 'admin', sysdate, 0	);
+INSERT INTO tbl_board VALUES (board_seq.nextval, '두 번째 글', '두 번째 내용', 'admin', sysdate, 0	);
+INSERT INTO tbl_board VALUES (board_seq.nextval, '세 번째 글', '세 번째 내용', 'admin', sysdate, 0	);
+INSERT INTO tbl_board VALUES (board_seq.nextval, '게시글 등록', '임현민', '임현민', sysdate, 0	);
 
+DELETE FROM TBL_BOARD ;
 
-
-
-
-
-
-
-
-
-
+SELECT count(*) FROM TBL_BOARD ;
+-- 페이징 처리
+SELECT b.* FROM
+	(SELECT rownum rn, a.* FROM
+	-- 서브 쿼리를 사용시 alias 가 반드시 있어야함
+	(SELECT * FROM TBL_BOARD ORDER BY BOARDNUM desc) a) b
+WHERE b.rn BETWEEN 1 AND 10;
 
 
 
